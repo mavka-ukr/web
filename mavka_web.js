30ff82b2620999f239ac36d1c50e3ed459e6fa74
КДБ <prykhozhdenko@gmail.com>
2026-06-29 19:20:03 +0300

    виправлення

--- mavka_web.js
+++ mavka_web.js
@@ -202,7 +202,9 @@ class Mavka {
   static async fetchAvailableVersions() {
     const url = `${window.MAVKA_WEB_URL}/версії/версії.txt`;
 
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      cache: 'no-store'
+    });
     const text = await response.text();
     const lines = text.trim().split("\n");
 
